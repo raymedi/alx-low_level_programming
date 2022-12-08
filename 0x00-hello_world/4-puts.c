9f1ac111019285bde575b9cfa26165fc2bc725fc
raymedi <rahmonoluwashola@gmail.com>
2022-12-08 14:20:04 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts
- *
- * Return: Always 0 (Success)
- */
+
+
+/**
+ *  * main -  Entry point 
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	    puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
+	    	
 }
