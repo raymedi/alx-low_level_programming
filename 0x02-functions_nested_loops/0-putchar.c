6f46c84c460ff633ef7052624138c46add70a53f
raymedi <rahmonoluwashola@gmail.com>
2022-12-13 18:32:11 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 /**
  * main - print_putchar
  * is written by ray
@@ -10,7 +10,7 @@ int main(void)
 
 {
 	_putchar('_');
-	_putchar('p);
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
