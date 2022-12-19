53b4ed8d777cefdc19375ca271f8cfbb66ddc7dc
raymedi <rahmonoluwashola@gmail.com>
2022-12-19 10:09:51 -0800

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,10 @@ void print_rev(char *s)
 
 	b = 0;
 
-	while (s[b] != '\n')
+	while (s[b] != '\0')
 		b++;
 
-	for (a = b - 1; a >= 0; 1--)
+	for (a = b - 1; a >= 0; a--)
 	{
 		_putchar(s[a]);
 	}
