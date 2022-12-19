9782afb5b90c7cd72da71055cbf1688ca3c2a8a2
raymedi <rahmonoluwashola@gmail.com>
2022-12-19 15:27:48 -0800

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,14 +11,14 @@ void puts_half(char *str)
 {
 	int a, n, longi;
 		longi = 0;
-for (a = 0; str[a] != '\0'; a++)
-	longi++;
+	for (a = 0; str[a] != '\0'; a++)
 
+		longi++;
 	n = (longi / 2);
-
 	if ((longi % 2) == 1)
-	n = ((longi + 1) / 2);
+	n = ((lingi + 1) / 2);
 	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
-	        _putchar('\n');
+	_putchar('\n');
+
 }
