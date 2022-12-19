257d25943f000e22724f4d6a4befebf1a9e1e861
raymedi <rahmonoluwashola@gmail.com>
2022-12-19 14:20:12 -0800

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * puts_half - a function that prints half of a string
@@ -13,6 +13,7 @@ void puts_half(char *str)
 		longi = 0;
 for (a = 0; str[a] != '\0'; a++)
 	longi++;
+
 	n = (longi / 2);
 	if ((longi % 2) == 1)
 		n = ((longi + 1) / 2);
