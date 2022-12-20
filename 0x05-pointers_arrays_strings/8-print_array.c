1c5c0a13af28ae32fb3ffe2130b568193a394abc
raymedi <rahmonoluwashola@gmail.com>
2022-12-19 16:55:52 -0800

    task9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,41 +2,21 @@
 
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- * Return: the int converted from the string
+ * print_array - a function that prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements OF the array to be printed
+ * Return: a and n inputs
  */
-
-int _atoi(char *s)
-
+void print_array(int *a, int n)
 {
-	int i, d, n, len, f, digit;
-		i = 0;
-		d = 0;
-		n = 0;
-		len = 0;
-		f = 0;
-		digit = 0;
-		while (s[len] != '\0')
-		len++;
-		while (i < len && f == 0)
+	int i;
+		for (i = 0; i < (n - 1); i++)
 	{
-			if (s[i] == '-')
-				++d;
-			if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-			digit = -digit;
-		n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + +1] > '9')
-				break;
-			f = 0;
-			}
-		i++;
+		printf("%d, ", a[i]);
 	}
-		if (f == 0)
-		return (0);
-		return (n);
+	if (i == (n - 1))
+	{
+		 printf("%d", a[n - 1]);
+	}
+	 printf("\n");
 }
