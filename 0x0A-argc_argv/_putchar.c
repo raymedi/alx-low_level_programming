4afa05ba1bb7c25d3ebd340391c288aefb33d342
raymedi <rahmonoluwashola@gmail.com>
2023-01-06 13:43:29 -0800

    task0

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#inckude "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: Ther character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and erno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
