69f3a2b4dd05954b344c44e0abca4eeac058df94
raymedi <rahmonoluwashola@gmail.com>
2023-01-16 15:09:59 -0800

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,7 +6,7 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- * Return: pointer to the new dog (Success), NULL otherwise
+ * @owner: owner of dog
  */
 dog_t *new_dog(char *name, float age, char *owner);
 {
