c4c07adb7e73f14c923287653b3051788a25988c
raymedi <rahmonoluwashola@gmail.com>
2023-01-16 15:05:04 -0800

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,9 +6,9 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- * @owner: owner of dog
+ * Return: the length of the string
  */
-dog_t *new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner);
 {
 	unsigned int nl, ol, i;
 	dog_t *dog;
