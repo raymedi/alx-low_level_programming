f7a8a38984cf02c58c4bff9564cf31bd5f4a0e97
raymedi <rahmonoluwashola@gmail.com>
2023-02-07 03:47:05 -0800

    FILE IO

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -7,23 +7,14 @@
 #include <stdlib.h>
 
 void check_elf(unsigned char *e_ident);
-
 void print_magic(unsigned char *e_ident);
-
 void print_class(unsigned char *e_ident);
-
 void print_data(unsigned char *e_ident);
-
 void print_version(unsigned char *e_ident);
-
 void print_abi(unsigned char *e_ident);
-
 void print_osabi(unsigned char *e_ident);
-
 void print_type(unsigned int e_type, unsigned char *e_ident);
-
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-
 void close_elf(int elf);
 
 /**
@@ -34,7 +25,7 @@ void close_elf(int elf);
 void check_elf(unsigned char *e_ident)
 {
 	int index;
-	for (index = 0; index < 4; index++)
+		for (index = 0; index < 4; index++)
 	{
 		if (e_ident[index] != 127 &&
 				e_ident[index] != 'E' &&
@@ -54,8 +45,8 @@ void check_elf(unsigned char *e_ident)
 void print_magic(unsigned char *e_ident)
 {
 	int index;
-	printf(" Magic: ");
-	for (index = 0; index < EI_NIDENT; index++)
+		printf(" Magic: ");
+		for (index = 0; index < EI_NIDENT; index++)
 	{
 		printf("%02x", e_ident[index]);
 		if (index == EI_NIDENT - 1)
