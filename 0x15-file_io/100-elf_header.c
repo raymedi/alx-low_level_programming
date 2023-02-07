2021a8e7c04ef27866850a08a1d78dc1a4ccb1ef
raymedi <rahmonoluwashola@gmail.com>
2023-02-07 03:41:31 -0800

    FILE IO

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -46,3 +46,21 @@ void check_elf(unsigned char *e_ident)
 		}
 	}
 }
+/**
+ *  print_magic - Prints the magic numbers of an ELF header
+ *  @e_ident: A pointer to an array containing the ELF magic numbers.
+ *  Description: Magic numbers are separated by spaces.
+ */
+void print_magic(unsigned char *e_ident)
+{
+	int index;
+	printf(" Magic: ");
+	for (index = 0; index < EI_NIDENT; index++)
+	{
+		printf("%02x", e_ident[index]);
+		if (index == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
