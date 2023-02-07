e9c575358679bc5d46f47bcd7b579f544b491806
raymedi <rahmonoluwashola@gmail.com>
2023-02-07 03:30:42 -0800

    FILE IO

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -31,4 +31,18 @@ void close_elf(int elf);
  * @e_ident: A pointer to an array containing the ELF magic numbers
  * Description: If the file is not an ELF file - exit code 98.
  */
-
+void check_elf(unsigned char *e_ident)
+{
+	int index;
+	for (index = 0; index < 4; index++)
+	{
+		if (e_ident[index] != 127 &&
+				e_ident[index] != 'E' &&
+				e_ident[index] != 'L' &&
+				e_ident[index] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
+}
